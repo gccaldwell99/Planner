b9c8530763de5c6c69498de7b6f37b034a3cb157
Galen Caldwell <gcc17@case.edu>
2016-03-26 15:08:27 -0400

    Added action stub classes
    Signed-off-by: Galen Caldwell <gcc17@case.edu>

---
+++ actions/Deposit.java
@@ -0,0 +1,25 @@
+package edu.cwru.sepia.agent.planner.actions;
+
+import edu.cwru.sepia.agent.planner.GameState;
+
+public class Deposit implements StripsAction {
+
+	@Override
+	public boolean preconditionsMet(GameState state) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public GameState apply(GameState state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
