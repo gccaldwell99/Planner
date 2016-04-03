d07a837443b56966f1e8dcaaf122c08b65b0543c
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2016-04-03 19:38:09 -0400

    Add k-strip support for Harvest.

--- actions/Harvest.java
+++ actions/Harvest.java
@@ -1,5 +1,9 @@
 package edu.cwru.sepia.agent.planner.actions;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import edu.cwru.sepia.action.Action;
 import edu.cwru.sepia.agent.planner.GameState;
 import edu.cwru.sepia.agent.planner.GameState.ResourceNodeWrapper;
@@ -9,24 +13,34 @@ import edu.cwru.sepia.environment.model.state.ResourceType;
 
 public class Harvest implements StripsAction {
 	private ResourceNodeWrapper resourceNode;
-	private WorkerWrapper worker;
+	private int k;
+	private List<WorkerWrapper> validWorkers;
 	
-	public Harvest(ResourceNodeWrapper resourceNode, WorkerWrapper worker) {
+	public Harvest(int k, HashMap<Integer, WorkerWrapper> workers, ResourceNodeWrapper resourceNode) {
 		this.resourceNode = resourceNode;
-		this.worker = worker;
+		this.k = k;
+		this.validWorkers = getValidWorkers(workers);
+	}
+	
+	private List<WorkerWrapper> getValidWorkers(HashMap<Integer, WorkerWrapper> workers) {
+		List<WorkerWrapper> validNeighbors = new ArrayList<WorkerWrapper>();
+		for (WorkerWrapper worker : workers.values()) {
+			// if any of the workers have a load then you shouldn't use them
+			if (worker.hasLoad)
+				continue;
+			// Check to see if all workers are next to the resource node you're trying to harvest
+			if (worker.position.equals(this.resourceNode.position))
+				validNeighbors.add(worker);
+		}
+		return validNeighbors;
 	}
 
 	@Override
 	public boolean preconditionsMet(GameState state) {
-		if(worker.hasLoad)
+		if(resourceNode.remainingResources< this.k*100)
 			return false;
-		
-		if(resourceNode.remainingResources<=0)
-			return false;
-		
-		// Check if your next to the resource your trying to harvest
-		return worker.position.equals(resourceNode.position);
-		
+				
+		return this.validWorkers.size() >= this.k;	
 	}
 
 	/**
@@ -35,16 +49,21 @@ public class Harvest implements StripsAction {
 	@Override
 	public GameState apply(GameState state) {
 		GameState newState = new GameState(state);
-		WorkerWrapper harvestingWorker = newState.workers.get(worker.id);
-		harvestingWorker.hasLoad = true;
-		if(resourceNode.type.equals(ResourceNode.Type.GOLD_MINE)) {
-			harvestingWorker.loadType = ResourceType.GOLD;
-		} else if(resourceNode.type.equals(ResourceNode.Type.TREE)) {
-			harvestingWorker.loadType = ResourceType.WOOD;
+		// shorten the valid workers to the size of k
+		this.validWorkers = new ArrayList<WorkerWrapper>(this.validWorkers.subList(0, k));
+		
+		for (WorkerWrapper worker : this.validWorkers) {
+			WorkerWrapper harvestingWorker = newState.workers.get(worker.id);
+			harvestingWorker.hasLoad = true;
+			if(resourceNode.type.equals(ResourceNode.Type.GOLD_MINE)) {
+				harvestingWorker.loadType = ResourceType.GOLD;
+			} else if(resourceNode.type.equals(ResourceNode.Type.TREE)) {
+				harvestingWorker.loadType = ResourceType.WOOD;
+			}
+			// Remove the resources from the new state
+			newState.resources.get(resourceNode.id).removeResources(100);
 		}
-	
-		// Remove the resources from the new state, then take it out of the priority queue if it is empty
-		newState.resources.get(resourceNode.id).removeResources(100);
+		// If resource is now empty, take it out of the priority queue
 		if(newState.closestTree.peek().remainingResources<=0) {
 			newState.closestTree.remove();
 		}
@@ -59,13 +78,29 @@ public class Harvest implements StripsAction {
 	
 	@Override
 	public Action getSepiaAction() {
-		return Action.createCompoundGather(worker.id, resourceNode.id);
+		if (this.validWorkers.size() > this.k) {
+			System.out.println("Attempt to get action before strip applied");
+			return null;
+		}
+		
+		// find one of the workers next to it
+		WorkerWrapper selectedWorker = this.validWorkers.get(0);
+		// remove it from being valid
+		// to prevent us from getting the same action on a later request
+		this.validWorkers.remove(selectedWorker);
+		
+		return Action.createCompoundGather(selectedWorker.id, resourceNode.id);
 	}
 
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Harvest from " + resourceNode.toString() + " with " + worker.toString());
-		return sb.toString();
+		sb.append("Harvest from " + resourceNode.toString() + " with ");
+		for (WorkerWrapper worker : this.validWorkers) {
+			sb.append(worker.toString()+ " and ");
+		}
+		String toReturn = sb.toString();
+		int andBegin = toReturn.length() - 5;
+		return toReturn.substring(0, andBegin);
 	}
 }
