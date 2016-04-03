dbf0906851d770a0e1e9ec31deba7c1ccac4e64d
Andrew Hamm <AndrewTurner.Hamm@gmail.com>
2016-04-03 18:33:46 -0400

    Update for k generic workers

--- actions/Deposit.java
+++ actions/Deposit.java
@@ -1,47 +1,50 @@
 package edu.cwru.sepia.agent.planner.actions;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import edu.cwru.sepia.action.Action;
 import edu.cwru.sepia.agent.planner.GameState;
 import edu.cwru.sepia.agent.planner.GameState.WorkerWrapper;
+import edu.cwru.sepia.agent.planner.Position;
 import edu.cwru.sepia.environment.model.state.ResourceType;
 
 public class Deposit implements StripsAction {
-	private List<WorkerWrapper> workers;
-	private int townhallID;
+	private List<WorkerWrapper> validWorkers;
+	private int townhallID, k;
+	private Position townhallLocation;
 	
-	public Deposit(List<WorkerWrapper> workers, int townhallID) {
-		this.workers = workers;
+	public Deposit(HashMap<Integer, WorkerWrapper> workers, int k, int townhallID, Position townhallLocation) {
+		this.validWorkers = getValidWorkers(workers);
 		this.townhallID = townhallID;
+		this.townhallLocation = townhallLocation;
+		this.k = k;
 	}
 
 	@Override
-	public boolean preconditionsMet(GameState state) {
-		boolean nextTo = true;
-		for (WorkerWrapper worker : workers) {
-			// if any of the workers don't have a load then you shouldn't use this
-			if (!worker.hasLoad)
-				return false;
-			// Check to see if all workers are next to the town hall your trying to deposit to
-			nextTo = nextTo && worker.position.equals(state.townhallLocation);
-		}
-		
-		return nextTo;
+	public boolean preconditionsMet(GameState state) {		
+		// if there are enough workers next to the town hall to
+		// execute k deposits then return true
+		return this.validWorkers.size() >= this.k;
 	}
 
 	@Override
 	public GameState apply(GameState state) {
 		GameState newState = new GameState(state);
-		for (WorkerWrapper worker : this.workers) {
-			WorkerWrapper harvestingWorker = newState.workers.get(worker.id);
+		
+		// shorten the valid workers to the size of k
+		this.validWorkers = new ArrayList<WorkerWrapper>(this.validWorkers.subList(0, k));
+		
+		for (WorkerWrapper worker : this.validWorkers) {
+			WorkerWrapper harvestingWorker = newState.workers.get(worker.id);	
 			
 			harvestingWorker.hasLoad = false;
 			harvestingWorker.loadType = null;
 			
-			if(worker.loadType.equals(ResourceType.GOLD)) {
+			if(harvestingWorker.loadType.equals(ResourceType.GOLD)) {
 				newState.obtainedGold+=100;
-			} else if(worker.loadType.equals(ResourceType.WOOD)) {
+			} else if(harvestingWorker.loadType.equals(ResourceType.WOOD)) {
 				newState.obtainedWood+=100;
 			}
 		}
@@ -50,16 +53,45 @@ public class Deposit implements StripsAction {
 		return newState;
 	}
 	
+	private List<WorkerWrapper> getValidWorkers(HashMap<Integer, WorkerWrapper> workers) {
+		List<WorkerWrapper> validNeighbors = new ArrayList<WorkerWrapper>();
+		for (WorkerWrapper worker : workers.values()) {
+			// if any of the workers don't have a load then you shouldn't use them
+			if (!worker.hasLoad)
+				continue;
+			// Check to see if all workers are next to the town hall your trying to deposit to
+			if (worker.position.equals(this.townhallLocation))
+				validNeighbors.add(worker);
+		}
+		return validNeighbors;
+	}
+	
 	@Override
 	public Action getSepiaAction() {
-		return Action.createCompoundDeposit(worker.id, townhallID);
+		if (this.validWorkers.size() > this.k) {
+			System.out.println("Attempt to get action before strip applied");
+			return null;
+		}
+		
+		// find one of the workers next to it
+		WorkerWrapper selectedWorker = this.validWorkers.get(0);
+		// remove it from being next to it
+		// to prevent us from getting the same action on a later request
+		this.validWorkers.remove(selectedWorker);
+		
+		return Action.createCompoundDeposit(selectedWorker.id, townhallID);
 	}
 	
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Deposit " + worker.loadType.toString() + " with " + worker.toString());
-		return sb.toString();
+		sb.append("Deposit ");
+		for (WorkerWrapper worker : this.validWorkers) {
+			sb.append(worker.loadType.toString()+" with "+worker.toString()+ " and ");
+		}
+		String toReturn = sb.toString();
+		int andBegin = toReturn.length() - 5;
+		return toReturn.substring(0, andBegin);
 	}
 
 }
